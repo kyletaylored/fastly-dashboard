8f4f7eddccd16f497285d63dae8b0c1cceaef42d
Kyle Taylor <kyletaylored@gmail.com>
2020-01-06 11:14:36 -0600

    Add in retries to limit the number of times we request data

--- geoip/index.js
+++ geoip/index.js
@@ -1,30 +1,30 @@
-const fs = require("fs");
-const path = require("path");
-const csv = require("fast-csv");
-const ipInfo = require("ipinfo");
-const cliProgress = require("cli-progress");
-const readline = require("readline");
-const Keyv = require("keyv");
-const program = require("commander");
-const typeOf = require("precise-typeof");
+const fs = require('fs');
+const path = require('path');
+const csv = require('fast-csv');
+const ipInfo = require('ipinfo');
+const cliProgress = require('cli-progress');
+const readline = require('readline');
+const Keyv = require('keyv');
+const program = require('commander');
+const typeOf = require('precise-typeof');
 
 // Get CLI options
 program
-  .option("-k, --key", "IPInfo authentication key")
-  .option("-i, --input [path]", "CSV input path")
-  .option("-o, --output [path]", "CSV output path");
+  .option('-k, --key', 'IPInfo authentication key')
+  .option('-i, --input [path]', 'CSV input path')
+  .option('-o, --output [path]', 'CSV output path');
 program.parse(process.argv);
 
 // Set up database.
-const db = new Keyv("sqlite://geoip.sqlite", { namespace: "geoip" });
+const db = new Keyv('sqlite://geoip.sqlite', { namespace: 'geoip' });
 // Handle DB connection errors
-db.on("error", err => {
-  console.log("Connection Error", err);
+db.on('error', err => {
+  console.log('Connection Error', err);
   process.exit(err);
 });
 
 // IP Info
-const ipinfoKey = program.key || "";
+const ipinfoKey = program.key || '';
 
 // Create progress bar
 const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
@@ -37,15 +37,12 @@ let filename, extname, outputFile;
 try {
   // Throw error for missing file input.
   if (file === undefined) {
-    throw Error(
-      "File not found, check the path or use an absolute path from the root."
-    );
+    throw Error('File not found, check the path or use an absolute path from the root.');
   }
   file = path.resolve(file);
   extname = path.extname(file);
   filename = path.basename(file, extname);
-  outputFile =
-    program.output || __dirname + "/results/" + filename + "-ipinfo.csv";
+  outputFile = program.output || __dirname + '/results/' + filename + '-ipinfo.csv';
 
   // Eventually, set total lines for CLI bar.
   getLines(file).then(lns => {
@@ -57,7 +54,7 @@ try {
 }
 
 // Create writer.
-const ws = fs.createWriteStream(outputFile).on("end", data => {
+const ws = fs.createWriteStream(outputFile).on('end', data => {
   ws.close();
 });
 
@@ -66,20 +63,17 @@ const rs = fs.createReadStream(file);
 rs.pipe(csv.parse({ headers: true }))
   .pipe(csv.format({ headers: true }))
   // Using the transform function from the formatting stream
-  .transform((row, next) => {
+  .transform(async (row, next) => {
     // Add IP data
-    this.pause();
-    if (row.hasOwnProperty("ip")) {
+    if (row.hasOwnProperty('ip')) {
       let ipinfo = getIpInfo(row.ip);
-      //   console.log(ipinfoKey);
       row = Object.assign({}, row, ipinfo);
     }
     bar.increment();
     return next(null, row);
-    this.resume();
   })
   .pipe(ws, { end: false })
-  .on("finish", () => {
+  .on('finish', () => {
     // Clean up.
     ws.destroy();
     bar.stop();
@@ -90,23 +84,25 @@ rs.pipe(csv.parse({ headers: true }))
  * Store and retrieve IP info.
  * @param {string} ip
  */
-function getIpInfo(ip) {
+async function getIpInfo(ip) {
   // Check for existing IP data.
-  ipcache = db.get(ip);
+  ipcache = await db.get(ip);
   let typed = typeOf(ipcache);
 
   // Fetch new data.
-  if (typed !== "object") {
-    ipcache = formatIpInfo(ip);
+  if (typed !== 'object' || (ipcache.hasOwnProperty('ip') && ipcache.ip == '')) {
+    ipcache = formatIpInfo(ip, ipcache);
   }
 
-  // Retry after checki ng for error response from IPinfo
+  // Retry after checking for error response from IPinfo
   //   if (typeof ipcache === "object" && ipcache.hasOwnProperty("error")) {
   //     console.warn(ipcache.error.title, ipcache.error.message);
   //   }
 
-  // Store results in db.
-  let ipdb = db.set(ip, ipcache);
+  // Store results
+  // If we have retries, set an expiration because we'll likely need to refetch
+  let ttl = ipcache.retry > 0 ? 10000 : null;
+  await db.set(ip, ipcache, ttl);
 
   return ipcache;
 }
@@ -115,32 +111,49 @@ function getIpInfo(ip) {
  * Extract and format IP info.
  * @param {string} ip
  */
-function formatIpInfo(ip) {
+function formatIpInfo(ip, cache) {
   // Create template.
   let ipdata = {
-    ip: "",
-    hostname: "",
-    city: "",
-    region: "",
-    country: "",
-    loc: "",
-    org: "",
-    postal: "",
-    lat: "",
-    lon: ""
+    ip: '',
+    hostname: '',
+    city: '',
+    region: '',
+    country: '',
+    loc: '',
+    org: '',
+    postal: '',
+    lat: '',
+    lon: '',
   };
 
+  // Update retries.
+  if (typeOf(cache) !== 'object') {
+    ipdata.retry = 0;
+  } else {
+    ipdata.retry = cache.retry + 1;
+  }
+
   // Fetch IP data.
-  ipInfo(ip, ipinfoKey, (err, info) => {
-    // Check if result is valid
-    if (typeof info == "object") {
-      // Split lat/lon for mapping.
-      if (info.hasOwnProperty("loc")) {
-        [info.lat, info.lon] = info.loc.split(",");
+  if (ipdata.retry > 4) {
+    ipInfo(ip, ipinfoKey, (err, info) => {
+      // Check if result is valid
+      if (typeof info == 'object') {
+        // Split lat/lon for mapping.
+        if (info.hasOwnProperty('loc')) {
+          [info.lat, info.lon] = info.loc.split(',');
+        }
+        ipdata = Object.assign({}, ipdata, info);
+
+        // Always overwrite IP address, just so we have data.
+        ipdata.ip = ip;
       }
-      ipdata = Object.assign({}, ipdata, info);
-    }
-  });
+      if (err) {
+        // console.warn(err);
+      }
+    });
+  } else {
+    // console.warn('Too many retries to fetch data. Let it die and try again later.');
+  }
 
   return ipdata;
 }
@@ -154,7 +167,7 @@ async function getLines(file) {
 
   const rl = readline.createInterface({
     input: fileStream,
-    crlfDelay: Infinity
+    crlfDelay: Infinity,
   });
   // Note: we use the crlfDelay option to recognize all instances of CR LF
   // ('\r\n') in input.txt as a single line break.
